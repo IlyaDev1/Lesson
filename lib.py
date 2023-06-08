0ef340511b638344290e962d4a89f7c527e12361
IliDevia <neiluamaibi@mail.ru>
2023-06-08 15:02:32 +0300

    firstcommit

---
+++ lib.py
@@ -0,0 +1,18 @@
+class Point:
+    __x = 0
+    __y = 0
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+        print(f'created: {self}')
+
+    def getCoor(self):
+        return (self.__x, self.__y)
+
+    def setCoor(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def __del__(self):
+        print(f'deleted: {self}')
